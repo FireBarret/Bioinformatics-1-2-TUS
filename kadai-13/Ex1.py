7b9273001c29e1e7acc7a9f13824d97ec5b9117e
FireBarret <74597153+FireBarret@users.noreply.github.com>
2023-07-04 11:07:39 +0900

    added kadai 13

--- kadai-13/Ex1.py
+++ kadai-13/Ex1.py
@@ -1,14 +1,22 @@
 #疑似コード
-procedure Ex1 begin
-    A=[6,2,1,5,3,0,5,6,5]
+'''##procedure Ex1 begin
+A =[6,2,1,5,3,0,5,6,5]
     i= 1
     x = 1
     while A[i] != 0
         x = x * A[i]
-        i= i+ 1
+        i= i + 1
     end while 
 end
-
+'''
+#リストに0が出るまで掛け算を繰り返す
 #最後にprint(x)でxの中身を表示
+A = [6,2,1,5,3,0,5,6,5]
+i = 0
+x = 1
+while A[i] != 0:
+   x = x * A[i]
+   i = i + 1
+#各段階を可視化するために以下のprintを追加した
+   print("Step " + str(i) + ": " + str(x))
 print(x)
-
--- kadai-13/Ex2.py
+++ kadai-13/Ex2.py
@@ -1,3 +1,4 @@
+'''
 #疑似コード
 procedure Ex2 begin
     A=[5,3,6,4]
@@ -9,8 +10,18 @@ procedure Ex2 begin
         end for
     end for
 end 
-
+'''
 
 #最後にprint(x)でxの中身を表示
+A = [5,3,6,4]
+B = [1,3,4]
+x = 0
+for i in range(4):
+    for j in range(3):
+        x = x + A[i] * B[j]
+#各段階を可視化するために以下のprintを追加した
+    print("Step " + str(i) + ": " + str(x))
 print(x)
+
+
 
